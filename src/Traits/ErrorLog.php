59311a60621d2928df555d20ab1f77494d1c84bf
DSchuppelius <daniel.schuppelius@gmail.com>
2025-03-16 10:00:48 +0100

    change logging

--- src/Traits/ErrorLog.php
+++ src/Traits/ErrorLog.php
@@ -11,63 +11,78 @@
 namespace ERRORToolkit\Traits;
 
 use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
 
 trait ErrorLog {
     protected ?LoggerInterface $logger = null;
 
-    protected function logDebug(string $message, array $context = []): void {
-        if (!is_null($this->logger)) {
-            $this->logger->debug($message, $context);
+    private static array $logLevelMap = [
+        LogLevel::EMERGENCY => LOG_EMERG,
+        LogLevel::ALERT     => LOG_ALERT,
+        LogLevel::CRITICAL  => LOG_CRIT,
+        LogLevel::ERROR     => LOG_ERR,
+        LogLevel::WARNING   => LOG_WARNING,
+        LogLevel::NOTICE    => LOG_NOTICE,
+        LogLevel::INFO      => LOG_INFO,
+        LogLevel::DEBUG     => LOG_DEBUG,
+    ];
+
+    /**
+     * Setzt einen PSR-3 kompatiblen Logger
+     */
+    public function setLogger(LoggerInterface $logger): void {
+        $this->logger = $logger;
+    }
+
+    /**
+     * Allgemeine Logging-Funktion mit PSR-3 Kompatibilität.
+     */
+    private function logMessage(string $level, string $message, array $context = []): void {
+        if ($this->logger) {
+            $this->logger->log($level, $message, $context);
         } else {
-            error_log("Debug: $message");
+            $logString = "[" . date('Y-m-d H:i:s') . "] [" . ucfirst($level) . "] $message";
+            if (ini_get('error_log')) {
+                error_log($logString);
+            } elseif (function_exists('syslog')) {
+                syslog(self::$logLevelMap[$level] ?? LOG_INFO, $message);
+            } else {
+                $tempDir = sys_get_temp_dir();
+                $logFile = $tempDir . DIRECTORY_SEPARATOR . "php-config-toolkit.log";
+                file_put_contents($logFile, $logString . PHP_EOL, FILE_APPEND);
+            }
         }
+    }
+
+    protected function logDebug(string $message, array $context = []): void {
+        $this->logMessage(LogLevel::DEBUG, $message, $context);
     }
 
     protected function logInfo(string $message, array $context = []): void {
-        if (!is_null($this->logger)) {
-            $this->logger->info($message, $context);
-        } else {
-            error_log("Info: $message");
-        }
+        $this->logMessage(LogLevel::INFO, $message, $context);
     }
 
     protected function logNotice(string $message, array $context = []): void {
-        if (!is_null($this->logger)) {
-            $this->logger->notice($message, $context);
-        } else {
-            error_log("Notice: $message");
-        }
+        $this->logMessage(LogLevel::NOTICE, $message, $context);
     }
 
     protected function logWarning(string $message, array $context = []): void {
-        if (!is_null($this->logger)) {
-            $this->logger->warning($message, $context);
-        } else {
-            error_log("Warning: $message");
-        }
+        $this->logMessage(LogLevel::WARNING, $message, $context);
     }
 
     protected function logError(string $message, array $context = []): void {
-        if (!is_null($this->logger)) {
-            $this->logger->error($message, $context);
-        } else {
-            error_log("Error: $message");
-        }
+        $this->logMessage(LogLevel::ERROR, $message, $context);
     }
 
     protected function logCritical(string $message, array $context = []): void {
-        if (!is_null($this->logger)) {
-            $this->logger->critical($message, $context);
-        } else {
-            error_log("Critical: $message");
-        }
+        $this->logMessage(LogLevel::CRITICAL, $message, $context);
     }
 
     protected function logAlert(string $message, array $context = []): void {
-        if (!is_null($this->logger)) {
-            $this->logger->alert($message, $context);
-        } else {
-            error_log("Alert: $message");
-        }
+        $this->logMessage(LogLevel::ALERT, $message, $context);
+    }
+
+    protected function logEmergency(string $message, array $context = []): void {
+        $this->logMessage(LogLevel::EMERGENCY, $message, $context);
     }
 }
